8e37f8ca35f671179fc904316ab8f3d8cf097d34
Jason Adams <julietmikealpha@gmail.com>
2018-06-27 10:17:27 +0100

    Initial checkin.

---
+++ IsVegan/Data/CanBeVegan.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsVegan.Data
+{
+  public class CanBeVegan
+  {
+    private List<string> data;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CanBeVegan"/> class.
+    /// </summary>
+    public CanBeVegan()
+    {
+      var assembly = Assembly.GetExecutingAssembly();
+      using (Stream stream = assembly.GetManifestResourceStream("IsVegan.Resources.canbevegan.json"))
+      {
+        using (StreamReader reader = new StreamReader(stream))
+        {
+          string json = reader.ReadToEnd();
+          data = JsonConvert.DeserializeObject<List<string>>(json);
+        }
+      }
+    }
+  }
+}
---
+++ IsVegan/Program.cs
@@ -0,0 +1,17 @@
+﻿using IsVegan.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsVegan
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      CanBeVegan canBeVegan = new CanBeVegan();
+    }
+  }
+}
