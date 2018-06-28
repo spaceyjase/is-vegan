581fd2b4132e7f046f38fce88d2b47fe0af0f74e
Jason Adams <julietmikealpha@gmail.com>
2018-06-28 14:29:05 +0100

    More tests.

--- IsVeganUnitTest/IsVeganTest.cs
+++ IsVeganUnitTest/IsVeganTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using IsVegan.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,10 +9,48 @@ namespace IsVeganUnitTest
   public class IsVeganTest
   {
     readonly IsVegan.Models.IsVegan isVegan = new IsVegan.Models.IsVegan();
+
     [TestMethod]
     public void TrueForEmptyString()
     {
       Assert.IsTrue(isVegan.IsVeganIngredient(string.Empty));
+    }
+
+    [TestMethod]
+    public void ShouldBeVeganIngredient()
+    {
+      Assert.IsTrue(isVegan.IsVeganIngredient("soy"));
+    }
+
+    [TestMethod]
+    public void ShouldNotBeVeganIngredient()
+    {
+      Assert.IsFalse(isVegan.IsVeganIngredient("milk"));
+    }
+
+    [TestMethod]
+    public void ShouldNotBeVeganIngredientList()
+    {
+      Assert.IsFalse(isVegan.IsVeganIngredientList(new string[]{ "aspid", "albumin" }));
+    }
+
+    [TestMethod]
+    public void ShouldBeVeganIngredientList()
+    {
+      Assert.IsTrue(isVegan.IsVeganIngredientList(new string[]{ "soy", "cacao butter" }));
+    }
+
+    [TestMethod]
+    public void ShouldNotContainNonVeganIngredientList()
+    {
+      Assert.IsTrue(isVegan.ContainsNonVeganIngredients(new string[] { "soy", "cacao butter" }).Length == 0);
+    }
+
+    [TestMethod]
+    public void ShouldContainNonVeganIngredientList()
+    {
+      Assert.IsTrue(isVegan.ContainsNonVeganIngredients(new string[] { "aspic", "albumin", "soy" })
+        .SequenceEqual(new string[] { "aspic", "albumin" }));
     }
   }
 }
