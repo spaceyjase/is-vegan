0c7af4a2804eaadb57d85aa390fb8a6436a7f476
Jason Adams <julietmikealpha@gmail.com>
2018-06-28 15:29:23 +0100

    Complete ingredient checker unit tests.

--- IsVeganUnitTest/IngredientCheckerUnitTest.cs
+++ IsVeganUnitTest/IngredientCheckerUnitTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using IsVegan.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace IsVeganUnitTest
@@ -6,9 +8,47 @@ namespace IsVeganUnitTest
   [TestClass]
   public class IngredientCheckerUnitTest
   {
+    private readonly IngredientChecker checkIngredients = new IngredientChecker();
+
     [TestMethod]
     public void ShouldContainFlaggedIngredientsButNoNonVeganIngredients()
     {
+      var result = checkIngredients.CheckIngredients(new string[]{
+        "biotin",
+        "glycine",
+        "soy",
+        "garlic"
+      });
+      Assert.IsTrue(result.NonVegan.Length == 0 && result.Flagged.SequenceEqual(new string[] { "biotin", "glycine" }));
+    }
+
+    [TestMethod]
+    public void ShouldNotContainFlaggedIngredientsButNoNonVeganIngredients()
+    {
+      var result = checkIngredients.CheckIngredients(new string[]{
+        "beef",
+        "pork",
+        "soy",
+        "garlic"
+      });
+      Assert.IsTrue(result.NonVegan.SequenceEqual(new string[] { "beef", "pork" }) && result.Flagged.Length == 0);
+    }
+
+    [TestMethod]
+    public void ShouldContainFlaggedAndNonVeganIngredients()
+    {
+      var result = checkIngredients.CheckIngredients(new string[]{
+        "beef",
+        "pork",
+        "glycine",
+        "biotin",
+        "soy",
+        "garlic"
+      });
+      Assert.IsTrue(
+        result.NonVegan.SequenceEqual(new string[] { "beef", "pork" }) &&
+        result.Flagged.SequenceEqual(new string[] { "glycine", "biotin" })
+      );
     }
   }
 }
